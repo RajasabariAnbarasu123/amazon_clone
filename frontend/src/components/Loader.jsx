44098bca0a5f894defe3868d08018f7b1b52f404
Rajasabari Anbarasu <2k22cse123@kiot.ac.in>
2026-01-31 14:37:23 +0530

    Delete frontend directory

--- frontend/src/components/Loader.jsx
+++
@@ -1,3 +0,0 @@
-export default function Loader() {
-  return <h3 style={{ textAlign: "center" }}>Loading...</h3>;
-}
--- frontend/src/pages/OrderSuccess.jsx
+++
@@ -1,9 +0,0 @@
-export default function OrderSuccess() {
-  return (
-    <div className="page">
-      <h2>✅ Order Placed Successfully</h2>
-      <p>Thank you for shopping with Amazon Clone!</p>
-      <p>Your payment was successful.</p>
-    </div>
-  );
-}
